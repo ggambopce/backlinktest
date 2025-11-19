c74bb78d3ca337ad27dbca184b5f17980a76e029
ggambo <ggambopce@gmail.com>
2025-11-19 18:31:40 +0900

    [FEAT] 웹소캣 채팅로직 테스트

--- src/app/routers/profile.py
+++ src/app/routers/profile.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from ..schemas.profile import ApiResponse, ProfileCreate
+from ..schemas.profile import ApiResponse, ProfileCreate, ProfileResponse
 from ..models.profile import Profile
 from ..core.database import get_db
 from sqlalchemy.orm import Session
@@ -15,9 +15,7 @@ def create_profile(
     payload: ProfileCreate,
     db: Session = Depends(get_db)
 ):
-    profile = Profile(
-        payload.dict(exclude_unset=True),
-    )
+    profile = Profile(**payload.dict(exclude_unset=True))
     db.add(profile)
     db.commit()
     db.refresh(profile)
@@ -25,20 +23,21 @@ def create_profile(
     return ApiResponse(
         code=200,
         message="프로필 작성 성공",
-        result=profile,
+        result=ProfileResponse.model_validate(profile).model_dump()
     )
 
 @router.get(
-    "/get",
+    "/{profile_id}",
     response_model=ApiResponse,
     status_code=status.HTTP_200_OK,
 )
 def get_profile(
+    profile_id: int,
     db: Session = Depends(get_db)
 ):
-    profile = db.query(Profile).all()
+    profile = db.get(Profile, profile_id)
     return ApiResponse(
         code=200,
         message="프로필 조회 성공",
-        result=profile,
+        result=ProfileResponse.model_validate(profile,from_attributes=True),
     )
--- src/app/schemas/profile.py
+++ src/app/schemas/profile.py
@@ -1,11 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional, Any
-from datetime import date
+from datetime import date, datetime
 
-class ApiResponse(BaseModel):
-    code: int
-    message: str
-    result: Optional[Any] = None
+
 
 class ProfileBase(BaseModel):
     profile_image_url: Optional[str] = None
@@ -20,5 +17,28 @@ class ProfileBase(BaseModel):
     sns_url: Optional[str] = None
     phone_number: Optional[str] = None
 
+class ProfileResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)  # Pydantic v2: orm_mode 대체
+
+    id: int
+    profile_image_url: str
+    temperament: str | None = None
+    enneagram: str | None = None
+    introduction: str | None = None
+    job: str | None = None
+    birth_date: date | None = None
+    gender: str | None = None
+    location: str | None = None
+    temperament_report: str | None = None
+    sns_url: str | None = None
+    phone_number: str | None = None
+    created_at: datetime
+    updated_at: datetime
+
 class ProfileCreate(ProfileBase):
     pass
+
+class ApiResponse(BaseModel):
+    code: int
+    message: str
+    result: ProfileResponse | None = None
